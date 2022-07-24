c3a7784a190d7af582fa644d453579bcac493c47
2044smile <2044smile@naver.com>
2022-07-24 23:12:45 +0900

    FIXME: 애매한 해결

--- account/views/account.py
+++ account/views/account.py
@@ -2,21 +2,15 @@ from drf_yasg.utils import swagger_auto_schema
 from django.utils.decorators import method_decorator
 from rest_framework import viewsets, status, permissions, mixins
 
-from account.core.exceptions import SignUpBadRequest, SignUpNotFound
 from account.models.account import Account
 from account.serializers.account import AccountSignUpSerializer
 
 
-@swagger_auto_schema(
+@method_decorator(name='create', decorator=swagger_auto_schema(
     operation_id='회원가입',
     operation_description='계정을 생성합니다.',
-    responses={
-        200: "OK",
-        400: SignUpBadRequest,  # FIXME: 정상작동하지 않음
-        404: SignUpNotFound  # FIXME: 정상작동하지 않음
-    },
     tags=['Account']
-)
+))
 class AccountSignUpViewSet(mixins.CreateModelMixin,
                            viewsets.GenericViewSet):
     permission_classes = (permissions.AllowAny, )
