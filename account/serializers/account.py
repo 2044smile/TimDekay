25d483dca646efb96d12dc41aa22f7bfcb9d6573
2044smile <2044smile@naver.com>
2022-07-26 23:48:59 +0900

    refactor: extra_kwargs, real_name and phone

--- account/serializers/account.py
+++ account/serializers/account.py
@@ -7,10 +7,13 @@ from phonenumber_field.serializerfields import PhoneNumberField
 class AccountSignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['email', 'password', 'nickname']
+        fields = ['email', 'password', 'nickname', 'real_name', 'phone']
         extra_kwargs = {
             "email": {"required": True},
-            "password": {"write_only": True, "required": True}
+            "password": {"write_only": True, "required": True},
+            "nickname": {"required": True},
+            "real_name": {"required": False},
+            "phone": {"required": True}
         }
 
 
