d11be8004d7740f0225c367747397c00048ccabc
2044smile <2044smile@naver.com>
2022-07-28 19:56:08 +0900

    feat: jwt token auth JWT

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Tim Dekay API",
@@ -34,7 +36,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('account.urls'))
+    path('', include('account.urls')),
+
+    path('jwt-token-auth/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('jwt-token-auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt-token-auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
