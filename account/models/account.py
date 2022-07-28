07ad04b27b51ba98d1c24f2160bb0ae4f28c2c6d
2044smile <2044smile@naver.com>
2022-07-28 23:06:28 +0900

    add comment

--- account/models/account.py
+++ account/models/account.py
@@ -33,6 +33,10 @@ class UserManager(BaseUserManager):
         )
         user.set_password(password)
         user.is_admin = True
+        # create_superuser 를 할 때 self 의 model 이 기본적으로 등록 된 데이터페이스를 보는 것 같습니다.
+        # 기본적으로 만들어진 phone 의 조건은 unique=True 어드민 계정을 생성할 때 사용되는 것은 create_superuser 이지만
+        # 관련이 있는 걸로 확인하였고, 아래의 구문을 추가하였습니다. + 어드민 계정을 만드는 경우에 돌아가는 코드이기 때문에
+        # 중복으로 인한 에러를 발견하기 어려울 것 입니다.
         lst = random.sample(range(1, 55), 5)  # I don't know create_superuser
         user.phone = "+" + str(lst)
         user.save(using=self._db)
