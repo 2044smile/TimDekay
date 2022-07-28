9b97020656dc353cb69328884d497625a2147acf
2044smile <2044smile@naver.com>
2022-07-28 19:57:22 +0900

    feat: create, login, info, password_reset create

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('health_check', views.health_check),
     path('account/phone', views.account_phone),
-    path('account/create', views.AccountSignUpViewSet.as_view({'post': 'create'}))
+    path('account/create', views.AccountSignUpViewSet.as_view({'post': 'create'})),
+    path('account/login', views.AccountLoginView.as_view()),
+    path('account/info/<int:pk>', views.AccountInfoView.as_view()),
+    path('account/password/reset/<int:pk>', views.PasswordResetView.as_view())
 ]
