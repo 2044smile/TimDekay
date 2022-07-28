cb16416b7ff469783a0917c9f919ced56610b24d
2044smile <2044smile@naver.com>
2022-07-28 23:08:05 +0900

    add comment

--- account/core/exceptions/exception.py
+++ account/core/exceptions/exception.py
@@ -1,6 +1,8 @@
 from rest_framework.exceptions import APIException
 
 
+# 해당 부분은 사용하려고 만들어뒀지만 개인 프로젝트에서는 딱히 필요가 없다고 느껴서 진행하지 않았습니다.
+# 회사의 경우엔 꼭 꼭 꼭 처리해야 될 것으로 보여집니다.
 class SignUpBadRequest(APIException):
     status_code = 400
     default_detail = "잘못된 요청입니다."
